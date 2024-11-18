625c3617b292bd68b09298e5f8bd0e599f5c22fa
IRFAN RATTHER <162337690+Irfan-rather@users.noreply.github.com>
2024-11-18 15:00:57 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,6 @@
 def dockerpush(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  withCredentials([usernamePassword(credentialsId: 'dockerHubcred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerHubPass}"
   }
   sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
